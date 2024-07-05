ac360c29f7adc6c3c9a96957d5575824e513215f
ARMANDA2022 <104578464+ARMANDA2022@users.noreply.github.com>
2024-07-05 15:22:25 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ import "./assets/img/4geeks.ico";
 
 //Aqui empieza mi codigo
 
+//creo mis variables con el texto que quiero que salga
+
 const who = ["The dog", "My grandma", "An alien", "My neighbor"];
 const action = ["ate", "broke", "stole", "hid"];
 const what = ["my homework", "the keys", "the car", "my shoe"];
@@ -17,8 +19,6 @@ const when = [
   "while I was sleeping"
 ];
 
-//crear una variable
-
 //crear la funcion para que genere la excusa random
 function generateExcuse() {
   let whoExcusa = who[Math.floor(Math.random() * who.length)];
@@ -26,18 +26,18 @@ function generateExcuse() {
   let whatExcusa = what[Math.floor(Math.random() * what.length)];
   let whenExcusa = when[Math.floor(Math.random() * when.length)];
 
-  //creo variable nueva y la pongo igual a la función
+  //creo variable nueva y la pongo igual a las variables que tiene mi función. para que se genere la excusa
 
   let fraCompleta =
     whoExcusa + " " + actionExcusa + " " + whatExcusa + " " + whenExcusa;
-
   document.querySelector("#palabras").innerHTML = fraCompleta;
 }
 
-const boton = document.getElementById("miBoton");
-
-boton.addEventListener("click", function() {});
-
 window.onload = function() {
+  //Creo mi boton para cuando haga click en el genere la excusa
+  const miBoton = document.getElementById("miBotonpalabras");
+  miBoton.addEventListener("click", generateExcuse);
+
+  // Generar una excusa inicial al cargar la página
   generateExcuse();
 };
